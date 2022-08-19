c1c0d263cb7b187e02fc9a9384c109b77df6acc9
Julien Lengrand-Lambert <julien@lengrand.fr>
2022-08-19 15:22:03 +0200

    Fixes eslint issues

--- src/main/js/pluckr-app/src/PluckrApp.js
+++ src/main/js/pluckr-app/src/PluckrApp.js
@@ -1,5 +1,5 @@
 import { LitElement, html, css } from 'lit';
-import './pluckr-login';
+import './pluckr-login.js';
 
 import '@vaadin/text-field';
 import '@vaadin/icons';
@@ -8,8 +8,8 @@ import '@vaadin/app-layout/vaadin-drawer-toggle.js';
 import '@vaadin/tabs';
 import '@vaadin/button';
 
-import mapboxgl from '../dist/mapbox-gl.esm.js'
-import MapboxGeocoder from '../dist/mapbox-gl-geocoder.esm.min.js'
+import mapboxgl from '../dist/mapbox-gl.esm.js';
+import MapboxGeocoder from '../dist/mapbox-gl-geocoder.esm.min.js';
 
 export class PluckrApp extends LitElement {
   map = null;
@@ -17,7 +17,7 @@ export class PluckrApp extends LitElement {
   static get properties() {
     return {
       title: { type: String },
-      location: {type: Object},
+      location: { type: Object },
     };
   }
 
@@ -46,9 +46,8 @@ export class PluckrApp extends LitElement {
         width: 1096px;
       }
 
-      pluckr-login{
+      pluckr-login {
         margin-left: auto;
-
       }
     `;
   }
@@ -56,24 +55,26 @@ export class PluckrApp extends LitElement {
   constructor() {
     super();
     this.title = 'Pluckr';
-    this.location = { x: 52.0474828687443, y: 5.080036739440433};
+    this.location = { x: 52.0474828687443, y: 5.080036739440433 };
   }
 
   firstUpdated(_changedProperties) {
     super.firstUpdated(_changedProperties);
 
     this.map = new mapboxgl.Map({
-      accessToken: 'pk.eyJ1IjoiamxlbmdyYW5kIiwiYSI6ImNsNWM3YTl3YjBla3ozYm8yMHo3NTRtbHkifQ.mhHRpOn0v-v59tXbvEYnlQ',
+      accessToken:
+        'pk.eyJ1IjoiamxlbmdyYW5kIiwiYSI6ImNsNWM3YTl3YjBla3ozYm8yMHo3NTRtbHkifQ.mhHRpOn0v-v59tXbvEYnlQ',
       container: this.renderRoot.querySelector('#map'),
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [this.location.y, this.location.x],
-      zoom: 13
+      zoom: 13,
     });
 
     this.map.addControl(
       new MapboxGeocoder({
-      accessToken: 'pk.eyJ1IjoiamxlbmdyYW5kIiwiYSI6ImNsNWM3YTl3YjBla3ozYm8yMHo3NTRtbHkifQ.mhHRpOn0v-v59tXbvEYnlQ',
-      mapboxgl: mapboxgl
+        accessToken:
+          'pk.eyJ1IjoiamxlbmdyYW5kIiwiYSI6ImNsNWM3YTl3YjBla3ozYm8yMHo3NTRtbHkifQ.mhHRpOn0v-v59tXbvEYnlQ',
+        mapboxgl,
       })
     );
 
@@ -82,19 +83,19 @@ export class PluckrApp extends LitElement {
     this.loadMarkers(this.map.getBounds());
   }
 
-  loadMarkers(bounds){
-    fetch(`/api/trees?bbox=${bounds._ne.lat},${bounds._ne.lng},${bounds._sw.lat},${bounds._sw.lng}`)
+  loadMarkers(bounds) {
+    fetch(
+      `/api/trees?bbox=${bounds._ne.lat},${bounds._ne.lng},${bounds._sw.lat},${bounds._sw.lng}`
+    )
       .then(response => response.json())
       .then(data => {
-        console.log('Loaded POIs', data);
         data.map(p =>
           new mapboxgl.Marker()
             .setLngLat([p.location.y, p.location.x])
             .addTo(this.map)
         );
-
       })
-      .catch((error) => {
+      .catch(error => {
         console.error('Impossible to log Points of Interests:', error);
       });
   }
@@ -111,8 +112,15 @@ export class PluckrApp extends LitElement {
         integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
         crossorigin=""
       />
-      <link href='https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css' rel='stylesheet' />
-      <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css" type="text/css">
+      <link
+        href="https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css"
+        rel="stylesheet"
+      />
+      <link
+        rel="stylesheet"
+        href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css"
+        type="text/css"
+      />
       <vaadin-app-layout>
         <vaadin-drawer-toggle slot="navbar touch-optimized">
         </vaadin-drawer-toggle>
